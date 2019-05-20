f89f663d5bd5a93e1bb9b24c9c1225c85c789ec1
yamamura-shin <yamamura-shin@DMM-MAC3-0063.local>
2019-05-20 18:57:22 +0900

    update リファクト

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/local/src/pyenv/versions/ecsdac/bin/python
+#!/usr/local/src/pyenv/versions/sonicom/bin/python
 
 """SMFT通信デモ
 Sound Modem
@@ -8,9 +8,13 @@ import struct
 
 import wave
 import numpy as np
+import matplotlib.pylab as plt
 
 from ecc.rsc import DataProcessor
 
+S_RATE      = 44100
+CHUNK_TIME  = 256
+CENTER_FREQ = 2000
 
 def main():
     dp = DataProcessor()
@@ -39,36 +43,41 @@ def main():
         d7 = c & 0x02
         d8 = c & 0x01
 
-        create_line_signal
 
-        l1.extend(create_line_signal(d1, 0))
-        l2.extend(create_line_signal(d2, 1))
-        l3.extend(create_line_signal(d3, 2))
-        l4.extend(create_line_signal(d4, 3))
-        l5.extend(create_line_signal(d5, 4))
-        l6.extend(create_line_signal(d6, 5))
-        l7.extend(create_line_signal(d7, 6))
-        l8.extend(create_line_signal(d8, 7))
+        l1.extend(create_line_signal(d1, 0, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l2.extend(create_line_signal(d2, 1, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l3.extend(create_line_signal(d3, 2, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l4.extend(create_line_signal(d4, 3, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l5.extend(create_line_signal(d5, 4, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l6.extend(create_line_signal(d6, 5, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l7.extend(create_line_signal(d7, 6, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
+        l8.extend(create_line_signal(d8, 7, a=0.2, cf=CENTER_FREQ, fs=S_RATE))
 
-        print(d1, d2, d3, d4, d5, d6, d7, d8)
 
-    # サイン波を-32768から32767の整数値に変換(signed 16bit pcmへ)
-    swav = np.array(l1) + np.array(l2) + np.array(l3) + np.array(l4) + np.array(l5) + np.array(l6) + np.array(l7) + np.array(l8)
+    swav = multiplex(l1, l2, l3, l4, l5, l6, l7, l8)
+
+    plt.plot(swav[:1024])
+
+    pcm_wav = to_pcm_struct(swav, fs=S_RATE)
+
+    fm_wav = []
 
     # FM変調
-    # for n in np.arange(t):
-    #     np.sin(2*np.pi*fc*t + (1000/44100) * swav)
+    for n in np.arange(len(swav)):
+        f_buf = np.sin(2 * np.pi * 2000 * len(swav) + (1000/2000) * swav[n])
 
-    swav = [int(x * 32767.0) for x in swav]
+        fm_wav.append(f_buf)
 
+    plt.plot(fm_wav[:1024])
+    plt.show()
 
 
     #バイナリ化
-    binwave = struct.pack("h" * len(swav), *swav)
+    binwave = struct.pack("h" * len(pcm_wav), *pcm_wav)
 
     #サイン波をwavファイルとして書き出し
     w = wave.Wave_write("output.wav")
-    p = (1, 2, 44100, len(binwave), 'NONE', 'not compressed')
+    p = (1, 2, CENTER_FREQ, len(binwave), 'NONE', 'not compressed')
     w.setparams(p)
     w.writeframes(binwave)
     w.close()
@@ -76,6 +85,21 @@ def main():
     upack_data = dp.unpack(packed_data)
 
     print(upack_data.decode())
+
+
+def multiplex(*lines):
+    formulas = []
+    for i in range(len(lines)):
+        formulas.append(f"np.array(lines[{i}])")
+
+    swav = eval(" + ".join(formulas))
+
+    return swav
+
+
+def to_pcm_struct(swav, fs):
+
+    return [int(x * (fs / 2.0)) for x in swav]
 
 
 def create_line_signal(data, linenum, a=0.2, cf=1200, fs=44100, t=256):
